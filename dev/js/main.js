e5e63ea9136c36ab43adfc48302fc52b2c5d6fc1
Narek <narek.tarverdyan@bk.ru>
2017-05-02 11:24:29 +0300

    fixes+

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
 	$('.home-slider').slick({
-		speed: 500,
+		speed: 700,
 		fade: true,
 		cssEase: 'linear',
+		autoplay: true,
+		autoplaySpeed: 5000,
 	});
 	$('.custom-scroll').perfectScrollbar({
 		maxScrollbarLength: 300,
